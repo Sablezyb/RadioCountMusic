bed51a92eb27bafae19a68a8af1730e99028a808
Roma <mig29.romakos@mail.ru>
2021-02-09 12:06:20 +0600

    first commit

---
+++ Radio music count/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Radio_music_count
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExclude = new System.Windows.Forms.Button();
+            this.btnRusMusic = new System.Windows.Forms.Button();
+            this.btnKazMusic = new System.Windows.Forms.Button();
+            this.btnRusReklama = new System.Windows.Forms.Button();
+            this.btnKazReklama = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(637, 389);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(152, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Посчитать";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbStatus.Location = new System.Drawing.Point(633, 72);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(155, 24);
+            this.lbStatus.TabIndex = 1;
+            this.lbStatus.Text = "Выберите папку";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(636, 140);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(152, 45);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Выбрать";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(615, 364);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnExclude
+            // 
+            this.btnExclude.Location = new System.Drawing.Point(633, 259);
+            this.btnExclude.Name = "btnExclude";
+            this.btnExclude.Size = new System.Drawing.Size(152, 45);
+            this.btnExclude.TabIndex = 4;
+            this.btnExclude.Text = "Исключить";
+            this.btnExclude.UseVisualStyleBackColor = true;
+            this.btnExclude.Click += new System.EventHandler(this.btnExclude_Click);
+            // 
+            // btnRusMusic
+            // 
+            this.btnRusMusic.Location = new System.Drawing.Point(886, 140);
+            this.btnRusMusic.Name = "btnRusMusic";
+            this.btnRusMusic.Size = new System.Drawing.Size(152, 45);
+            this.btnRusMusic.TabIndex = 5;
+            this.btnRusMusic.Text = "Рус Муз";
+            this.btnRusMusic.UseVisualStyleBackColor = true;
+            this.btnRusMusic.Click += new System.EventHandler(this.btnRusMusic_Click);
+            // 
+            // btnKazMusic
+            // 
+            this.btnKazMusic.Location = new System.Drawing.Point(886, 259);
+            this.btnKazMusic.Name = "btnKazMusic";
+            this.btnKazMusic.Size = new System.Drawing.Size(152, 45);
+            this.btnKazMusic.TabIndex = 6;
+            this.btnKazMusic.Text = "Каз Муз";
+            this.btnKazMusic.UseVisualStyleBackColor = true;
+            this.btnKazMusic.Click += new System.EventHandler(this.btnKazMusic_Click);
+            // 
+            // btnRusReklama
+            // 
+            this.btnRusReklama.Location = new System.Drawing.Point(1093, 140);
+            this.btnRusReklama.Name = "btnRusReklama";
+            this.btnRusReklama.Size = new System.Drawing.Size(152, 45);
+            this.btnRusReklama.TabIndex = 7;
+            this.btnRusReklama.Text = "Рус Реклама";
+            this.btnRusReklama.UseVisualStyleBackColor = true;
+            this.btnRusReklama.Click += new System.EventHandler(this.btnRusReklama_Click);
+            // 
+            // btnKazReklama
+            // 
+            this.btnKazReklama.Location = new System.Drawing.Point(1093, 259);
+            this.btnKazReklama.Name = "btnKazReklama";
+            this.btnKazReklama.Size = new System.Drawing.Size(152, 45);
+            this.btnKazReklama.TabIndex = 8;
+            this.btnKazReklama.Text = "Каз Реклама";
+            this.btnKazReklama.UseVisualStyleBackColor = true;
+            this.btnKazReklama.Click += new System.EventHandler(this.btnKazReklama_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 450);
+            this.Controls.Add(this.btnKazReklama);
+            this.Controls.Add(this.btnRusReklama);
+            this.Controls.Add(this.btnKazMusic);
+            this.Controls.Add(this.btnRusMusic);
+            this.Controls.Add(this.btnExclude);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExclude;
+        private System.Windows.Forms.Button btnRusMusic;
+        private System.Windows.Forms.Button btnKazMusic;
+        private System.Windows.Forms.Button btnRusReklama;
+        private System.Windows.Forms.Button btnKazReklama;
+    }
+}
+
---
+++ Radio music count/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Radio_music_count
+{
+    public partial class Form1 : Form
+    {
+        List<string> listRusMusic = new List<string>();
+        List<string> listKazMusic = new List<string>();
+        //List<string> listRusReklama = new List<string>();
+        //List<string> listKazReklama = new List<string>();
+        List<string> listExclude = new List<string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] paths = { "rus Music.txt", "kaz Music.txt", "no Used.txt" };
+            List<List<string>> lists = new List<List<string>>
+            {
+                listRusMusic,
+                listKazMusic,
+                listExclude
+            };
+
+            for (int i = 0; i<paths.Length; i++)
+            {
+                GetInfoInFile(paths[i], lists[i]);
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+
+                using (StreamReader sr = new StreamReader(path, Encoding.ASCII))
+                {
+                    string separ1 = @"\Music\каз\";
+                    //string separ2 = @"\Реклама\";
+                    string separ3 = @"^\\Music\\(\d+\.\d+\.\d+)\\";
+                    string separ4 = @"^\d+\s";
+                    string separ5 = @"\|+.+";
+                    Regex regex = new Regex(separ3);
+                    Regex regex2 = new Regex(separ4);
+                    Regex regex3 = new Regex(separ5);
+                    
+                    string str;
+                    while ((str = sr.ReadLine())!= null)
+                    {
+                        string newStr = "";
+                        if (str.StartsWith(separ1))
+                        {
+                            newStr = str.Replace(separ1, "");
+                            if (Regex.IsMatch(newStr, separ5))
+                            {
+                                newStr = regex3.Replace(newStr, "");
+
+                                if (IsFind(listKazMusic, newStr))
+                                {
+                                    newStr = "";
+                                }
+                            }
+                        }
+                        else if (Regex.IsMatch(str, separ3))
+                        {
+                            newStr = regex.Replace(str, "");
+                            if (Regex.IsMatch(newStr, separ4))
+                            {
+                                newStr = regex2.Replace(newStr, "");
+                                if (Regex.IsMatch(newStr, separ5))
+                                {
+                                    newStr = regex3.Replace(newStr, "");
+
+                                    if (IsFind(listExclude, newStr))
+                                    {
+                                        newStr = "";
+                                    }
+                                    else if (IsFind(listRusMusic, newStr))
+                                    {
+                                        newStr = "";
+                                    }
+                                    else
+                                    {
+                                        AddInfoInFile("rus Music.txt", newStr);
+                                        listRusMusic.Add(newStr);
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if (newStr != "")
+                        {
+                            listBox1.Items.Add(newStr);
+                        }
+
+                    }
+                }
+
+
+                btnStart.Enabled = true;
+            }
+        }
+
+        bool IsFind(List<string> list, string strFind)
+        {
+            for (int i = 0; i<list.Count; i++)
+            {
+                if (list[i] == strFind)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        void AddInfoInFile(string path, string info)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, Encoding.Default))
+            {
+                sw.WriteLine(info);
+            }
+        }
+
+        void GetInfoInFile(string path, List<string> list)
+        {
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                string str;
+                while ((str = sr.ReadLine()) != null)
+                {
+                    list.Add(str);
+                }
+            }
+        }
+
+        private void btnExclude_Click(object sender, EventArgs e)
+        {
+            string info = listBox1.Items[listBox1.SelectedIndex].ToString();
+            AddInfoInFile("no Used.txt", info);
+        }
+
+        private void btnRusMusic_Click(object sender, EventArgs e)
+        {
+            string info = listBox1.Items[listBox1.SelectedIndex].ToString();
+            AddInfoInFile("rus Music.txt", info);
+        }
+
+        private void btnKazMusic_Click(object sender, EventArgs e)
+        {
+            string info = listBox1.Items[listBox1.SelectedIndex].ToString();
+            AddInfoInFile("kaz Music.txt", info);
+        }
+
+        private void btnRusReklama_Click(object sender, EventArgs e)
+        {
+            string info = listBox1.Items[listBox1.SelectedIndex].ToString();
+            AddInfoInFile("rus Reklama.txt", info);
+        }
+
+        private void btnKazReklama_Click(object sender, EventArgs e)
+        {
+            string info = listBox1.Items[listBox1.SelectedIndex].ToString();
+            AddInfoInFile("kaz Reklama.txt", info);
+        }
+    }
+}
